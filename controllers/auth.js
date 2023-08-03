f593bf4247ea55095ab5daa15dd7a722817a338d
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-08-03 16:17:00 +0300

    creating gravatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,14 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const gravatar = require("gravatar");
+const crypto = require("crypto");
+const path = require("path");
+const fs = require("fs/promises");
+
 const { User } = require("../models/user");
 const { HttpError, wrapper } = require("../helpers");
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const { SECRET_KEY } = process.env;
 
@@ -14,8 +20,16 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  // const emailHash = crypto.createHash('md5').update(email).digest('hex');
+  // const avatarURL = `https://www.gravatar.com/avatar/${emailHash}.jpg?d=monsterid`;
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
@@ -73,10 +87,23 @@ const updateSubscriptionUser = async (req, res) => {
   res.json({ email, subscription });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`
+  const resultUpload = path.join(avatarDir, fileName);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", fileName);
+  await User.findByIdAndUpdate(_id, avatarURL);
+
+  res.json({ avatarURL });
+};
+
 module.exports = {
   register: wrapper(register),
   login: wrapper(login),
   getCurrent: wrapper(getCurrent),
   logout: wrapper(logout),
   updateSubscriptionUser: wrapper(updateSubscriptionUser),
+  updateAvatar: wrapper(updateAvatar),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,13 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const bodyLengthCheck = require("./bodyLengthCheck");
-const authenticate = require('./authenticate')
+const authenticate = require('./authenticate');
+const upload = require("./upload");
 
 module.exports = {
   isValidId,
   validateBody,
   bodyLengthCheck,
   authenticate,
+  upload,
 };
